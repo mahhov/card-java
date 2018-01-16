c8f12c16949898ef64f9f912cf1abbbcd6f96cda
m <mahhov1@gmail.com>
2018-01-16 12:09:11 -0800

    primary craft

--- public/app.js
+++ public/app.js
@@ -1,21 +1,48 @@
 let item = {};
 
 let baseProperties = {
-    fire: {minValue: 1, maxValue: 101, name: 'health regen'},
-    water: {minValue: 1, maxValue: 101, name: 'shield'},
-    air: {minValue: 1, maxValue: 101, name: 'shield regen'},
-    earth: {minValue: 1, maxValue: 101, name: 'health'},
+    fire: {minValue: 10, maxValue: 101, name: 'health regen'},
+    water: {minValue: 10, maxValue: 101, name: 'shield'},
+    air: {minValue: 10, maxValue: 101, name: 'shield regen'},
+    earth: {minValue: 10, maxValue: 101, name: 'health'},
+};
+
+let valueBonus = 50;
+
+let createItemProperty = (source, bonus, maxMult) => {
+    let property = baseProperties[source];
+    return {name: property.name, value: bonus + randInt(property.minValue, property.maxValue * maxMult), source: source};
 };
 
 let createBaseProperty = (elements) => {
-    if (elements.length === 1) {
-        let property = baseProperties[elements[0]];
-        return {name: property.name, value: randInt(property.minValue, property.maxValue)};
+    if (elements.length === 1)
+        return createItemProperty(elements[0], 0, 1);
 
-    } else {
+    else {
         let index = randInt(0, 2);
-        let property = baseProperties[elements[index]];
-        return {name: property.name, value: 25 + randInt(property.minValue, property.maxValue)};
+        return createItemProperty(elements[index], valueBonus, 1);
+    }
+};
+
+let createPrimaryProperty = (glows) => {
+    let index = randInt(0, 3);
+    let elements = glows[index].elements;
+
+    if (elements.length === 1)
+        return createItemProperty(elements[0], 0, .5);
+
+    else if (elements[0] === elements[1])
+        return createItemProperty(elements[0], 0, 1);
+
+    else {
+        let index = randInt(0, 2);
+        if (item.step === 2)
+            if (item.property[1].source === elements[0])
+                index = 1;
+            else if (item.property[1].source === elements[1])
+                index = 0;
+
+        return createItemProperty(elements[index], 0, .5);
     }
 };
 
@@ -55,12 +82,32 @@ let stepBase = () => {
     else
         item.property[0] = createBaseProperty(glows[0].elements);
 
-    item.step = 1;
+    item.step++;
     controller.updateProperties(item);
 };
 
 let stepPrimary = () => {
+    if (item.step !== 1 && item.step !== 2)
+        return;
 
+    let glows = controller.getGlows();
+
+    if (glows.length !== 3)
+        return;
+
+    if (item.step === 2) {
+        let repeat = _.find(glows, (glow) => {
+            let elements = glow.elements;
+            return (elements.length === 1 || elements[0] === elements[1]) && item.property[1].source === elements[0]
+        });
+        if (repeat)
+            return;
+    }
+
+    item.property[item.step] = createPrimaryProperty(glows);
+
+    item.step++;
+    controller.updateProperties(item);
 };
 
 let stepSecondary = () => {
@@ -75,8 +122,8 @@ window.onload = init;
 
 /*
  item type determines secondary attributes possible
- base gives rolls 1-100 based on which glow
- primary 1 and 2 rolls 1-50 of randomly chosen from 3 glows
+ base gives rolls 10-100 based on which glow for tier 1, 60-150 for tier 2 or hybrid (chosen randomly)
+ primary 1 and 2 rolls 10-50 (1st glow) or 10-100 (2nd glow) or 10-50 or random glow (hybrid glow) of randomly chosen from 3 glows
  primary clear removes primary enchants and reduces enchant-ability by 5 
  secondary 1 and 2 rolls rolls of secondary attributes, possible outcomes based on which glows, value range based on # of glows  
  secondary clear removes secondary enchants and reduces enchant-ability by 10 
